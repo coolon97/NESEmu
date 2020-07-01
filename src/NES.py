7eacbad87a13db0b46b7b0dacb14e58f8b3ba1e2
coolon97 <hiroaki.takeo.ut@gmail.com>
2020-07-01 10:31:39 +0900

    opcode.py rename "oplist.py"

--- src/NES.py
+++ src/NES.py
@@ -5,14 +5,16 @@ import nesio
 import ppu
 import apu
 import bus
-import pygame
+'''import pygame'''
+from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2.QtUiTools import QUiLoader
 import sys
 
 
 class NES:
     def __init__(self):
         self.reset()
-    
+
     def reset(self):
         self.io = nesio.NesIO()
         self.ram = ram.RAM()
@@ -23,30 +25,42 @@ class NES:
 
     def load(self, data):
         self.rom = rom.ROM(data)
-        
+
     def start(self):
         self.bus = bus.BUS(self.ram, self.rom, self.ppu, self.apu, self.io)
         self.cpu = cpu.CPU(self.bus)
         self.cpu.start()
 
-
+'''
 if __name__ == "__main__":
     pygame.init()
     pygame.display.set_mode((320, 240))
     screen = pygame.display.get_surface()
     pygame.display.set_caption("test")
 
-    bg = pygame.image.load("../assets/test.png").convert_alpha()
+    bg = pygame.image.load("../assets/test.png")
     rect = bg.get_rect()
 
     while True:
         screen.fill((0, 0, 0))
-        screen.blit(bg, (0, 0))
+        screen.blit(bg, rect)
         pygame.display.update()
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-    
+'''
 
+
+class UISmaple(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(UISmaple, self).__init__(parent)
+        self.resize(400, 300)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    a = UISmaple()
+    a.show()
+    sys.exit()
